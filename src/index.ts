de035edbb7122601211940894a6022efe23a8635
Jirlan <jirlansouza08@gmaial.com>
2022-08-06 13:42:46 -0400

    Created the swagger onpenAPI 3.0 documentation in project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swaggerFile.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export { app };
