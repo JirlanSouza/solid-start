0cd91eeb166d1bd14d4525ef30f2335053ed5660
Jirlan <jirlansouza08@gmaial.com>
2022-08-05 22:53:32 -0400

    Changed the all class to pass in to tets

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const existUser = this.usersRepository.findByEmail(email);
+
+    if (existUser) {
+      throw new Error("user already exists");
+    }
+
+    const createdUser = this.usersRepository.create({ name, email });
+
+    return createdUser;
   }
 }
 
